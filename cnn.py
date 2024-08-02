93536fcfb37bc5e0b3951ab0217bee1e3356a9c5
Sebastian Xiao <30613810+xiaobh2010@users.noreply.github.com>
2024-08-02 15:41:47 +0800

    Add files via upload

--- cnn.py
+++ cnn.py
@@ -41,6 +41,7 @@ import torchvision
 def get_model(num_classes):
     model = torchvision.models.resnet18(pretrained=True)
     for param in model.parameters():
+        #冻结除最后一层以外的所有层
         param.requires_grad = False
     model.fc = torch.nn.Sequential(
         torch.nn.Dropout(p=0.3),
@@ -48,30 +49,41 @@ def get_model(num_classes):
     )
     return model
 
-model = get_model(num_classes=17)
+#加载权重
+def load_model(num_classes, pth):
+    # model = torchvision.models.resnet18(pretrained=False)
+    # num_ftrs = model.fc.in_features
+    # model.fc = nn.Linear(num_ftrs, num_classes)
+    # model.load_state_dict(torch.load(pth))
+    model = torch.load(pth)
+    return model
+
+
+# model = get_model(num_classes=17)
+model = load_model(num_classes=17, pth=r'Acc0.9897058823529412.pth')
 print(model)
 
 #====================================================================
 num_classes = 17
-batch_size = 1
+batch_size = 4
 learning_rate = 0.01
-num_epochs = 1000
+num_epochs = 100
 #====================================================================
 
 data_transform = {
     "train": transforms.Compose([
-                transforms.Resize((224, 224)),
+                transforms.Resize((256, 256)),
                 transforms.RandomHorizontalFlip(p=0.5),
                 # transforms.RandomRotation(10),
                 transforms.ToTensor(),
-                # transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                #                      std=[0.229, 0.224, 0.225])
+                transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225])
     ]),
     "val": transforms.Compose([
-                transforms.Resize((224, 224)),
+                transforms.Resize((256, 256)),
                 transforms.ToTensor(),
-                # transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                #                      std=[0.229, 0.224, 0.225]),
+                transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225]),
     ])
 }
 
@@ -114,7 +126,7 @@ print(f'{flower_list}')
 model.to(device=device)
 
 loss_function = nn.CrossEntropyLoss()
-optimizer = optim.SGD(model.parameters(), lr=learning_rate)
+optimizer = optim.SGD(model.parameters(), lr=learning_rate, momentum=0.9)
 
 model=model.cuda()
 
@@ -136,57 +148,65 @@ for epoch in range(num_epochs):
     time_start = time.time()
     print(f"Epoch {epoch+1}/{num_epochs}")
     model.train()
-    for step, data in tqdm(enumerate(iter(train_loader))):
+    #1360（train dataset image number）/batch_size
+    for step, data in tqdm(enumerate(train_loader), desc='train loader'):
     # for step, data in tqdm(enumerate(iter(tr))):
         x, y = data
         x, y = x.cuda(), y.cuda()
         # print(x.shape,  y.shape)
         # print(f"{images}---{labels}")
-        # optimizer.zero_grad()
+
+        optimizer.zero_grad()
         y_pred = model(x)
         # print(f'y_pred: {y_pred.shape}')
-        _, pred = torch.max(y_pred, 1)
+
 
         # out_prob = torch.softmax(pred, dim=-1)
-        topk_prob_values, topk_class_indices = torch.topk(y_pred, k=5, dim=-1)
-        print(f'topk_prob_values: {topk_prob_values}')
+        # topk
+        # topk_prob_values, topk_class_indices = torch.topk(y_pred, k=5, dim=-1)
+        # print(f'topk_prob_values: {topk_prob_values}')
 
-        loss = loss_function(y_pred, pred)
-        acc_count = torch.sum( pred ==y ).item()
+        loss = loss_function(y_pred, y)
 
         loss.backward()
         optimizer.step()
-        optimizer.zero_grad()
+        # optimizer.zero_grad()
 
         loss = loss.cpu()
         # acc = acc.cpu()
-        train_loss+=loss
+        train_loss+=loss.item()
+
+
+        _, pred = torch.max(y_pred, 1)
+        acc_count = torch.sum(pred == y).item()
         train_acc+=acc_count
 
         # train_loss.append(loss.data)
         # train_acc.append(acc)
-    print(f'train_acc: {train_acc}')
-    time_end = time.time()
-    # print(train_loss, train_acc)
-    # print(f'batch:{batch_size},train_loss:{sum(train_loss)/len(train_loss)/batch_size:.4f},train_Acc:{sum(train_acc)/len(train_acc)/batch_size:.4f},Time: {time_end-time_start}')
-    print(f'batch:{step},train_loss:{train_loss/ len(train_loader) / batch_size:.4f},train_Acc:{train_acc/ len(train_loader)/batch_size:.4f},Time: {time_end - time_start}')
-    # epoch_loss = running_loss * batch_size / len(train_dataset)
-    # epoch_acc = 100 * running_corrects / len(train_dataset)
 
+        time_end = time.time()
+        # print(train_loss, train_acc)
+        # print(f'batch:{batch_size},train_loss:{sum(train_loss)/len(train_loss)/batch_size:.4f},train_Acc:{sum(train_acc)/len(train_acc)/batch_size:.4f},Time: {time_end-time_start}')
+        print(f'epoch:{epoch},iter:{step+1},train_loss:{train_loss/ len(train_loader) / batch_size:.4f},Time: {time_end - time_start}')
+        # epoch_loss = running_loss * batch_size / len(train_dataset)
+        # epoch_acc = 100 * running_corrects / len(train_dataset)
+    print(f'train_acc: {train_acc}')
+    print(f'train_Acc: {100*train_acc / len(train_loader) / batch_size: .4f}%')
+    train_loss, train_acc = 0, 0
     #=========================================================================================================================================================
     # model.train(False)
     model.eval()
     with torch.no_grad():
-        for step, data in tqdm(enumerate(iter(val_loader))):
+        for step, data in tqdm(enumerate(val_loader), desc='val loader'):
             x, y = data
             x, y = x.cuda(), y.cuda()
             # print(f'{y}')
 
             y_pred = model(x)
+
+            loss = loss_function(y_pred, y)
+
             _, pred = torch.max(y_pred, 1)
-
-            loss = loss_function(y_pred, pred)
-
             acc_count = torch.sum( pred ==y ).item()
 
             loss=loss.cpu()
@@ -195,15 +215,18 @@ for epoch in range(num_epochs):
             # test_acc.append(acc)
 
             test_loss += loss
-            test_acc += acc_count
+            # test_acc += acc_count
+            test_acc+= acc_count
+
         # print(f'batch:{batch_size},train_loss:{sum(test_loss)/len(test_loss)/batch_size:.4f},train_Acc:{sum(test_acc)/len(test_acc)/batch_size:.4f},Time: {time_end - time_start}')
         # test_acc_value = sum(test_acc)/len(test_acc)/batch_size
-        print(f'batch:{step},test_loss:{test_loss/len(val_loader)/batch_size:.4f},test_Acc:{test_acc/len(val_loader)/batch_size:.4f},Time: {time_end - time_start}')
-        test_acc_value = test_acc/len(val_loader)/batch_size
-        if test_acc_value>best_acc:
-            best_acc = test_acc_value
-            pth_name= "Acc" + str(best_acc) + ".pth"
-            torch.save(model, pth_name)
+    print(f'epoch:{epoch},test_loss:{test_loss/len(val_loader)/batch_size:.4f},test_Acc:{100*test_acc/len(val_loader)/batch_size:.4f}%,Time: {time_end - time_start}')
+    test_acc_value = test_acc/len(val_loader)/batch_size
+    if test_acc_value>best_acc:
+        best_acc = test_acc_value
+        pth_name= "Acc" + str(best_acc) + ".pth"
+        print('Best checkpoint and save it ...!!!')
+        torch.save(model, pth_name)
 
 
 
